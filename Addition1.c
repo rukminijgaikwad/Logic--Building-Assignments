cbdf69bb12547b01a2c2d28936f977c4610c8d2c
rukminijgaikwad <rukminig592@gmail.com>
2025-05-10 19:01:27 +0530

    Add files via upload

---
+++ Addition1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int no1=10;
+    int no2=21;
+    int Ans=0; //default value of int is 0
+
+    Ans = no1+no2; //expression 
+     
+    printf("Addition is :%d\n",Ans);   //cu handle 
+
+    return 0;  
+}
